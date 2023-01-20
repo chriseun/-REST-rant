dd4a8fe9934e20cdcce3a510fb9577456182b5e8
Chris Eun <eunchristopher@gmail.com>
2023-01-20 02:01:58 -0800

    testing

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,14 @@
+// const router = require('express').Router()
+
+// router.get('/', (req, res) =>{
+//   res.send('GET /places')
+// })
+
+// module.exports = router
 const router = require('express').Router()
 
-router.get('/', (req, res) =>{
-  res.send('GET /places')
+router.get('/', (req, res) => {
+    res.send('GET /places')
 })
 
 module.exports = router
