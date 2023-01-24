659f681c455b681579778423a24cbe4498e0bc16
Chris Eun <eunchristopher@gmail.com>
2023-01-23 19:57:20 -0800

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-require('dotenv').config()
+// require('dotenv').config()
 
+// const express = require('express')
+// const app = express()
+
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+
+// app.use('/places', require('./controllers/places'))
+
+// app.get('/', (req, res) => {
+//     res.render('home')
+//     // res.send('Hello world!')
+// })
+
+// app.get('/', (req, res) => {
+//     res.render('places/index')
+//     // res.send('Hello world!')
+// })
+
+// app.get('*', (req, res) => {
+//     // res.status(404).render('error404')
+//     render('error404')
+// })
+
+// app.listen(process.env.PORT)
+// Modules and Globals
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
-    // res.send('Hello world!')
-})
-
-app.get('/', (req, res) => {
-    res.render('places/index')
-    // res.send('Hello world!')
 })
 
 app.get('*', (req, res) => {
-    // res.status(404).render('error404')
     res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
