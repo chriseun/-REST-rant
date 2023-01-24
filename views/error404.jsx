7c261500cf2cde54d2fae5148a281eaf06fd8b7f
Chris Eun <eunchristopher@gmail.com>
2023-01-23 20:20:05 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
       <main>
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
+        <img src="/images/404" alt="404" />
+        <div>
+          Photo by <a href="https://unsplash.com/@niuhang">hang niu</a> on <a href="https://unsplash.com/">UNSPLASH</a>
+        </div>
       </main>
     </Def>
   )
--- views/home.jsx
+++ views/home.jsx
@@ -2,14 +2,22 @@ const React = require('react')
 const Def = require('./default')
 
 function home () {
-  return (
-    <Def>
-        <main>
-            <h1>HOME</h1>
-        </main>
-    </Def>
-  )
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                  <div>
+                    Photo by <a href="https://unsplash.com/@good_bear">Morton Xiong</a> on <a href="https://unsplash.com/">UNSPLASH</a>
+                  </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places to Rant or Rave About</button>
+                </a>
+            </main>
+        </Def>
+    )
 }
-
 
 module.exports = home
